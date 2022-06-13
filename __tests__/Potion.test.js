8949bdd431e5fc88a27f2f3c575d84e491883385
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-06-13 14:25:50 -0400

    progressing

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -7,11 +7,10 @@ test('creates a health potion object', () => {
   expect(potion.value).toEqual(expect.any(Number));
 });
 
-test('creates a random potion object', () => {
+test('creates a random potion object', () => { 
   const potion = new Potion();
 
   expect(potion.name).toEqual(expect.any(String));
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
-
